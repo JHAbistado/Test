fd79ecc70bc87b1462cde17fd89c28fe928f0460
Dan <dandanronda25@gmail.com>
2025-08-21 03:44:16 +0800

    Modern Slider (YT tutorial)
    https://www.youtube.com/watch?v=VUtJ7FWCfZA

---
+++ Controllers/ExperimentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MHD.Controllers
+{
+    public class ExperimentController : Controller
+    {
+        // GET: Experiment
+        public ActionResult modern_slider()
+        {
+            return View();
+        }
+    }
+}
